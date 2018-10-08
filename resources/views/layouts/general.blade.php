f5a97c7d7760e856410cde59e903e857130e05bd
tigomiddelkoop <tigo.middelkoop@gmail.com>
2018-10-08 13:46:56 +0200

    Added a Open Graph thing to display fancy text in Discord and other platforms

--- resources/views/layouts/general.blade.php
+++ resources/views/layouts/general.blade.php
@@ -5,6 +5,11 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta property="og:title" content="GenericDevelopment">
+    <meta property="og:site_name" content="GenericDevelopment">
+    <meta property="og:site_url" content="https://genericdevelopment.nl">
+    <meta property="og:description" content="I make Generic applications for Generic people">
+    <meta property="og:image" content="https://images.genericdevelopment.nl/logoGD">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ asset('css/main-style.css') }}" rel="stylesheet" type="text/css">
     @yield('css')
