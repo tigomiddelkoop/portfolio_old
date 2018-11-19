5378494487da2c386aac18d9e098a2dc5b32e3f0
tigomiddelkoop <tigo.middelkoop@gmail.com>
2018-11-19 16:38:16 +0100

    Disabled the Carousel indicators

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -27,11 +27,11 @@
             </div>
             <div class="col-md-6">
                 <div id="carousel" class="carousel slide carousel-fade" data-ride="carousel">
-                    <ol class="carousel-indicators">
-                        <li data-target="#carousel" data-slide-to="0" class="active"></li>
-                        <li data-target="#carousel" data-slide-to="1"></li>
-                        <li data-target="#carousel" data-slide-to="2"></li>
-                    </ol>
+                    {{--<ol class="carousel-indicators">--}}
+                        {{--<li data-target="#carousel" data-slide-to="0" class="active"></li>--}}
+                        {{--<li data-target="#carousel" data-slide-to="1"></li>--}}
+                        {{--<li data-target="#carousel" data-slide-to="2"></li>--}}
+                    {{--</ol>--}}
                     <div class="carousel-inner">
                         @foreach($project->picture as $picture)
                             @if($picture->first_picture == TRUE)
