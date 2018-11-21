20e6b6afcae434c71efd029658e7e30fea12abb7
tigomiddelkoop <tigo.middelkoop@gmail.com>
2018-11-21 18:08:20 +0100

    working on the skills admiin page

--- resources/views/admin/skills/index.blade.php
+++ resources/views/admin/skills/index.blade.php
@@ -3,7 +3,8 @@
 @section('content')
     <div class="row">
         @foreach($codelanguages as $codelanguage)
-            <div class="col-md-3">
+            <div class="col-md-4 col-xs-12 col-sm-6 col-lg-3">
+            {{--<div class="col-md-4 col-xs-12 col-sm-6 col-lg-3">--}}
                 <div class="box">
                     <div class="box-header">
                         <h3 class="box-title">{{ $codelanguage->language }}</h3>
@@ -20,27 +21,38 @@
                             </tr>
                             @foreach($codelanguage->skills as $skills)
                                 <tr>
+
                                     <th>{{ $skills->skill }}</th>
                                     <th>
-                                        <div class="material-switch">
-                                            @if($skills->learned == 1)
-                                                <input id="{{ $codelanguage->language }}{{ $skills->skill }}" name=""
-                                                       type="checkbox" checked/>
-                                            @else
-                                                <input id="{{ $codelanguage->language }}{{ $skills->skill }}" name=""
-                                                       type="checkbox"/>
-                                            @endif
-                                            <label for="{{ $codelanguage->language }}{{ $skills->skill }}"
-                                                   class="label-success"></label>
-                                        </div>
+                                        <form action method="POST">
+                                            @csrf
+                                            <div class="material-switch">
+                                                @if($skills->learned == 1)
+                                                    <input id="{{ $codelanguage->language }}{{ $skills->skill }}"
+                                                           name="{{ $skills->id }}"
+                                                           type="checkbox" checked/>
+                                                @else
+                                                    <input id="{{ $codelanguage->language }}{{ $skills->skill }}"
+                                                           name="{{ $skills->id }}"
+                                                           type="checkbox"/>
+                                                @endif
+                                                <label for="{{ $codelanguage->language }}{{ $skills->skill }}"
+                                                       class="label-success"></label>
+                                            </div>
+                                        </form>
                                     </th>
                                 </tr>
                             @endforeach
                             <tr>
-                                <th></th>
-                                <th>
-                                    <button type="submit" class="btn btn-primary" style="width: 100%">Add</button>
-                                </th>
+                                <form action="" method="">
+                                    @csrf
+                                    <th>
+                                        <input type="text" class="form-control">
+                                    </th>
+                                    <th>
+                                        <button type="submit" class="btn btn-primary" style="width: 100%">Add</button>
+                                    </th>
+                                </form>
                             </tr>
                         </table>
                     </div>
