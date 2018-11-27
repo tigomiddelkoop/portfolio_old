87f5787b9e1fec9d64dff16de6a53d3fefef9944
tigomiddelkoop <tigo.middelkoop@gmail.com>
2018-11-27 13:52:48 +0100

    Updated dashboard to fix a small mistake i made

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,39 +1,41 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="col-md-3">
-        <div class="info-box">
-            <span class="info-box-icon bg-aqua"><i class="fa fa-users"></i></span>
-            <div class="info-box-content">
-                <span class="info-box-text">Users Registered</span>
-                <span class="info-box-number">1</span>
+    <div class="row">
+        <div class="col-md-3">
+            <div class="info-box">
+                <span class="info-box-icon bg-aqua"><i class="fa fa-users"></i></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">Users Registered</span>
+                    <span class="info-box-number">1</span>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-md-3">
-        <div class="info-box">
-            <span class="info-box-icon bg-aqua"><i class="fa fa-files-o"></i></span>
-            <div class="info-box-content">
-                <span class="info-box-text">Forum Topics</span>
-                <span class="info-box-number">0</span>
+        <div class="col-md-3">
+            <div class="info-box">
+                <span class="info-box-icon bg-aqua"><i class="fa fa-files-o"></i></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">Forum Topics</span>
+                    <span class="info-box-number">0</span>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-md-3">
-        <div class="info-box">
-            <span class="info-box-icon bg-aqua"><i class="fa fa-envelope-o"></i></span>
-            <div class="info-box-content">
-                <span class="info-box-text">Unread Messages</span>
-                <span class="info-box-number">1</span>
+        <div class="col-md-3">
+            <div class="info-box">
+                <span class="info-box-icon bg-aqua"><i class="fa fa-envelope-o"></i></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">Unread Messages</span>
+                    <span class="info-box-number">1</span>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-md-3">
-        <div class="info-box">
-            <span class="info-box-icon">Oi</span>
-            <div class="info-box-content">
-                <span class="info-box-text">Users Registered</span>
-                <span class="info-box-number">1</span>
+        <div class="col-md-3">
+            <div class="info-box">
+                <span class="info-box-icon">Oi</span>
+                <div class="info-box-content">
+                    <span class="info-box-text">Users Registered</span>
+                    <span class="info-box-number">1</span>
+                </div>
             </div>
         </div>
     </div>
